632b63b93a8307dfc57c3bf05549bd11f03667f0
salsabilarusdi <salsabilaaca584@gmail.com>
2026-06-11 15:33:19 +0700

    Create pseudocode.c

---
+++ kumpulan-tugas/pseudocode.c
@@ -0,0 +1,15 @@
+PSEUDOCODE
+
+Algoritma Menentuan_Kelulusan
+(Menentukan nilai apakah mahasiswa lulus atau tidak)
+
+Kamus
+n : integer
+
+Deskripsi
+read(n)
+if n mod > = 50 then
+write (“ANDA GAGAL”)
+else
+write (“ANDA LULUS”)
+endif
